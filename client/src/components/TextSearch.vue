7bc72cea63fa85874efeeacc16fb013bfbd91008
pakuyuya <pakuyuya@yahoo.co.jp>
2019-02-04 23:48:06 +0900

    mod client * add TextSearch.vue * install sass-loader

---
+++ client/src/components/TextSearch.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="outermost-box">
+    <div class="left-control"></div>
+    <div class="detail"></div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'TestSearch'
+}
+</script>
+
+<style scoped lang="scss">
+
+.outermost-box {
+    display: flex;
+    display: -webkit-flex;
+    flex-direction: row;
+}
+
+.left-control {
+    display: flex;
+    display: -webkit-flex;
+    flex-direction: columns;
+    flex-grow: 1;
+}
+
+.detail {
+    display: flex;
+    display: -webkit-flex;
+    flex-direction: columns;
+    flex-grow: 1;
+}
+
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import TextSearch from '@/components/TextSearch'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'TextSearch',
+      component: TextSearch
     }
   ]
 })
